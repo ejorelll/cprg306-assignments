edd4d2bc15b3123322efb33c385d86644e534da2
ejorelll <joreltao123@gmail.com>
2024-09-24 14:49:27 -0600

    assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+    <h1>Tobias/Toby Tao</h1>
+    <Link href="https://github.com/ejorelll" target="_blank">
+    https://github.com/ejorelll
+    </Link>
+  </div>
+  );
+}   
